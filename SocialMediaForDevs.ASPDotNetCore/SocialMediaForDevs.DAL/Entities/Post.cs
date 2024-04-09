40955e8de35fd51a057638f77a7b8052ee6ec954
quantruong158 <quantruong150803@gmail.com>
2024-04-09 16:10:32 +0700

    Update Post entity with new attributes: ImgUrl and CodeSnippet

--- SocialMediaForDevs.ASPDotNetCore/SocialMediaForDevs.DAL/Entities/Post.cs
+++ SocialMediaForDevs.ASPDotNetCore/SocialMediaForDevs.DAL/Entities/Post.cs
@@ -1,11 +1,17 @@
-﻿namespace SocialMediaForDevs.DAL.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialMediaForDevs.DAL.Entities;
 
 public class Post
 {
     public int Id { get; set; }
+    [Column(TypeName = "text")]
     public string Content { get; set; } = default!;
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; }
+    public string ImgUrl { get; set; } = default!;
+    [Column(TypeName = "text")]
+    public string CodeSnippet { get; set; } = default!;
     public int UserId { get; set; }
     public User User { get; set; } = default!;
     public List<Comment> Comments { get; set; } = [];
---
+++ SocialMediaForDevs.ASPDotNetCore/SocialMediaForDevs.DAL/Migrations/20240409075733_UpdatePostAttributes.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaForDevs.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePostAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CodeSnippet",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "Posts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodeSnippet",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "Posts");
+        }
+    }
+}
