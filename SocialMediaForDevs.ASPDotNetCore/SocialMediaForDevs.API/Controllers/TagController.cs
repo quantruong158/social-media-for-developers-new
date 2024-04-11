a3c5b6eaa92d691156a54397b325696c9bbd4640
quantruong158 <quantruong150803@gmail.com>
2024-04-11 22:15:02 +0700

    Add TagController and configure dependency injection

---
+++ SocialMediaForDevs.ASPDotNetCore/SocialMediaForDevs.API/Controllers/TagController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMediaForDevs.BLL.Services.Interfaces;
+
+namespace SocialMediaForDevs.API;
+
+[ApiController]
+[Route("api/tags")]
+public class TagController(ITagService _tagService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetTags()
+    {
+        var tags = await _tagService.GetTagsAsync();
+        return Ok(tags);
+    }
+
+    [HttpGet("{tagId}")]
+    public async Task<IActionResult> GetTagById(int tagId)
+    {
+        var tag = await _tagService.GetTagByIdAsync(tagId);
+        return Ok(tag);
+    }
+}
--- SocialMediaForDevs.ASPDotNetCore/SocialMediaForDevs.API/Program.cs
+++ SocialMediaForDevs.ASPDotNetCore/SocialMediaForDevs.API/Program.cs
@@ -1,4 +1,5 @@
 using SocialMediaForDevs.DAL;
+using SocialMediaForDevs.BLL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddBLL();
 builder.Services.AddDAL(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
